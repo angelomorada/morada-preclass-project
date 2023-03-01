f7a4c80f972fead2a7514f2211b54f1230b90e58
Angelo Morada <dummyplaytore.jp@gmail.com>
2023-03-01 17:12:59 +0800

    Activity 2: Full name hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def hash_full_name(full_name)
+  name_str = ''
+  full_name.each do |type,name|
+    name_str = name_str + name + ' '
+  end
+
+  return name_str
+
+end
+
+p hash_full_name({first_name: 'Angelo Jeo', middle_name: 'Sabao', last_name:'Morada'})
